bdc286d5447e892107ae8f9b786b7f771baef8a3
Lukas <graiglukas1@gmail.com>
2022-09-16 08:59:23 -0400

    refactored box form

--- storganize/forms.py
+++ storganize/forms.py
@@ -2,10 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, FieldList
 from wtforms.validators import DataRequired
 
-class CreateBoxForm(FlaskForm):
+class BoxForm(FlaskForm):
     box_title = StringField('title', validators=[DataRequired()])
     box_desc = StringField('desc')
     box_type = StringField('box_type')
-
-class AddItemForm(FlaskForm):
-    item = FieldList(StringField('item'))
