442e2148de54bd73bbb426bb4147aef6dc74b0f4
Lukas <graiglukas1@gmail.com>
2022-09-17 14:09:06 -0400

    added CreateUser for user reg

--- storganize/forms.py
+++ storganize/forms.py
@@ -1,8 +1,14 @@
+from wsgiref.validate import validator
 from flask_wtf import FlaskForm
-from wtforms import StringField, FieldList
+from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired
 
 class BoxForm(FlaskForm):
     box_title = StringField('title', validators=[DataRequired()])
     box_desc = StringField('desc')
     box_type = StringField('box_type')
+
+class CreateUser(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
